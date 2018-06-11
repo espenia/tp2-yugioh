1a08abfcb5f4d0ff288d6b404ea08ee7b8e9502a
archaine <32021777+archaine@users.noreply.github.com>
2018-06-11 23:30:18 +0000

    missing refactor from CartaMonstruo

--- src/test/testMano.java
+++ src/test/testMano.java
@@ -9,7 +9,7 @@ public class testMano {
     @Test
     public void testAgregarCartaAMano(){
         Mazo mazo = new Mazo();
-        Monstruo carta1 = new Monstruo("Dragon negro de Ojos Rojos",2400,2700);
+        CartaMonstruo carta1 = new CartaMonstruo("Dragon negro de Ojos Rojos",2400,2700);
         mazo.agregarCarta(carta1);
         Mano mano = new Mano(mazo,new Campo());
         mano.extraerCarta();
